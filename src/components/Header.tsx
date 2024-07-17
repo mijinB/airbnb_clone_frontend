fcc0002a660411bb54f61310bafd8b3106fe2d48
Bmijin <bmj13465@jtts.co.kr>
2024-07-17 13:41:27 +0900

    theme mode 설정(theme mode의 함수, 태그들 사용_주석참고)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,47 @@
-import { Box, Button, HStack, IconButton, useDisclosure } from "@chakra-ui/react";
-import { FaAirbnb, FaMoon } from "react-icons/fa";
+import {
+    Box,
+    Button,
+    HStack,
+    IconButton,
+    LightMode,
+    useColorMode,
+    useColorModeValue,
+    useDisclosure,
+} from "@chakra-ui/react";
+import { FaAirbnb, FaMoon, FaSun } from "react-icons/fa";
 import LoginModal from "./LoginModal";
 import SignUpModal from "./SignUpModal";
+import { Link } from "react-router-dom";
 
 export default function Header() {
     const { isOpen: isLoginOpen, onClose: onLoginClose, onOpen: onLoginOpen } = useDisclosure();
     const { isOpen: isSignUpOpen, onClose: onSignUpClose, onOpen: onSignUpOpen } = useDisclosure();
+    const { colorMode, toggleColorMode } = useColorMode();
+    const logoColor = useColorModeValue("red.500", "red.200");
+    // color는 상관없지만, 컴포넌트는 대문자로 시작해야된다.
+    const Icon = useColorModeValue(FaMoon, FaSun);
 
     return (
         <HStack justifyContent={"space-between"} py={5} px={10} borderBottomWidth={1}>
-            <Box color={"red.500"}>
-                <FaAirbnb size={"48"} />
+            <Box color={logoColor}>
+                <Link to={"/"}>
+                    <FaAirbnb size={"48"} />
+                </Link>
             </Box>
             <HStack spacing={"2.5px"}>
-                <IconButton variant={"ghost"} aria-label={"Toggle dark mode"} icon={<FaMoon />} />
+                <IconButton
+                    onClick={toggleColorMode}
+                    variant={"ghost"}
+                    aria-label={"Toggle dark mode"}
+                    icon={<Icon />}
+                />
                 <Button onClick={onLoginOpen}>Log in</Button>
-                <Button onClick={onSignUpOpen} colorScheme={"red"}>
-                    Sign up
-                </Button>
+                {/** dark mode일 때, 자동으로 연한 색상으로 변경해주는데 그냥 light mode일 때의 색상을 유지하고 싶다면 LightMode 태그 사용 */}
+                <LightMode>
+                    <Button onClick={onSignUpOpen} colorScheme={"red"}>
+                        Sign up
+                    </Button>
+                </LightMode>
             </HStack>
             <LoginModal isOpen={isLoginOpen} onClose={onLoginClose} />
             <SignUpModal isOpen={isSignUpOpen} onClose={onSignUpClose} />
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
+import theme from "./theme";
 
-const root = ReactDOM.createRoot(
-    document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
     <React.StrictMode>
-        <ChakraProvider>
+        <ChakraProvider theme={theme}>
+            {/** 사용자가 theme mode를 선택한 후 새로고침하면, 다시 초기화 mode로 돌아오기 때문에 사용자가 선택한 theme을 유지하기 위해 ColorModeScript 태그 사용 */}
+            <ColorModeScript initialColorMode={theme.config.initialColorMode} />
             <RouterProvider router={router} />
         </ChakraProvider>
     </React.StrictMode>
