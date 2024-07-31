968f5a18b1245839800632a51e5d312bae88a034
mijinB <bmj13465@gmail.com>
2024-07-31 22:53:40 +0900

    CSRF Token 보내기 & log out 기능 구현 & useQueryClient(refetchQueries)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,6 +21,7 @@ import SignUpModal from "./SignUpModal";
 import { Link } from "react-router-dom";
 import useUser from "../lib/useUser";
 import { logOut } from "../api";
+import { useQueryClient } from "@tanstack/react-query";
 
 export default function Header() {
     const { userLoading, user, isLoggedIn } = useUser();
@@ -30,23 +31,23 @@ export default function Header() {
     const logoColor = useColorModeValue("red.500", "red.200");
     const Icon = useColorModeValue(FaMoon, FaSun);
     const toast = useToast();
+    const queryClient = useQueryClient();
 
     const onLogOut = async () => {
-        // const data = await logOut();
-        // console.log(data);
-
         const toastId = toast({
             title: "Log out...",
             description: "Sad to see you go...",
             status: "loading",
         });
-        setTimeout(() => {
-            toast.update(toastId, {
-                status: "success",
-                title: "Done!",
-                description: "See you later!",
-            });
-        }, 5000);
+
+        await logOut();
+        queryClient.refetchQueries({ queryKey: ["me"] });
+
+        toast.update(toastId, {
+            status: "success",
+            title: "Done!",
+            description: "See you later!",
+        });
     };
 
     return (
