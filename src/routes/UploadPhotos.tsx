0be85056628204413fb51e745d3456afb86da3b9
mijinB <bmj13465@gmail.com>
2024-09-12 22:41:27 +0900

    upload한 image url 가져오기

--- src/routes/UploadPhotos.tsx
+++ src/routes/UploadPhotos.tsx
@@ -3,10 +3,25 @@ import useHostOnlyPage from "../components/HostOnlyPage";
 import ProtectedPage from "../components/ProtectedPage";
 import { useForm } from "react-hook-form";
 import { useParams } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
+import { getUploadURL } from "../api";
+
+interface IForm {
+    file: FileList;
+}
 
 export default function UploadPhotos() {
-    const { register, watch } = useForm();
+    const { register, handleSubmit } = useForm<IForm>();
+    const mutation = useMutation({
+        mutationFn: getUploadURL,
+        onSuccess: (data: any) => {
+            console.log(data);
+        },
+    });
     const { roomPk } = useParams();
+    const onSubmit = (data: any) => {
+        mutation.mutate();
+    };
     useHostOnlyPage();
 
     return (
@@ -14,11 +29,11 @@ export default function UploadPhotos() {
             <Box pb={40} mt={10} px={{ base: 10, lg: 40 }}>
                 <Container>
                     <Heading textAlign={"center"}>Upload a Photo</Heading>
-                    <VStack spacing={5} mt={10}>
+                    <VStack as={"form"} onSubmit={handleSubmit(onSubmit)} spacing={5} mt={10}>
                         <FormControl>
                             <Input {...register("file")} type="file" accept="image/*" />
                         </FormControl>
-                        <Button w="full" colorScheme={"red"}>
+                        <Button type="submit" w="full" colorScheme={"red"}>
                             Upload photos
                         </Button>
                     </VStack>
